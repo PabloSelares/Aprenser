89b1a47179f26dad4c9d9746419197dec0846315
Pablo Selares <pabloselares@gmail.com>
2024-07-26 11:27:44 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const midPhoto = document.querySelector('.mid-photo');
+    const textMid = document.querySelector('.text-mid');
+   
+   
+    midPhoto.classList.add('show');
+    textMid.classList.add('show');
+});
